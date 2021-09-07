88fec0351c444df831a9eb3da3a1e7c0038eea64
Ostyn Sy <osy304@insite.4cd.edu>
2021-09-06 21:12:55 -0700

    better formated the intro

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,36 +9,52 @@ const Home = () => {
     document.title = "Ostyn Sy"
   }, [])
 
+
+
   return (
     
     <div class="container">
       
       <div class="topnav">
         <img id="logo-image" src={process.env.PUBLIC_URL + "images/logo.png"} width="40" height="40"/>
-        <a href="#sec-intro">About</a>
         <a href="#sec-exp">Experience</a>
         <a href="#sec-proj">Projects</a>
         <a href="#sec-res">Resume</a>
       </div>
 
-      <section id="sec-intro">
-        <img id="intro-image" src={process.env.PUBLIC_URL + "/images/me.jpg"} width="300" height="400"/>
-        <div id="intro-title"><strong>Hello!</strong></div>
-        <div id="intro-name">My name is <strong>Ostyn Sy</strong>.</div>
-        <div id="intro-desc">I'm a fulltime student at <strong>San Francisco State University</strong> working on my <strong>Computer Science Major</strong>. Currently, I am focused on Software Engineering as a <strong>Fullstack Developer</strong>.</div>
-        <button id="button-contactme"> Contact Me</button>
-      </section>
 
+      <section id="intro-sec">
+        <div id="intro-grid-container">
+          <img id="intro-image" src={process.env.PUBLIC_URL + "/images/me.jpg"} width="240" height="320"/>
+          <div id="intro-title"><strong>Hello!</strong></div>
+          <div id="intro-name">My name is <strong>Ostyn Sy</strong>.</div>
+          <div id="intro-desc">I'm a full-time student at <strong>San Francisco State University</strong> working on my <strong>Computer Science Major</strong>. Currently, I am focused on Software Engineering as a <strong>Fullstack Developer</strong>.</div>
+          <a id="button-contactme" href="mailto:ostynsy@gmail.com"> Contact Me</a>
+        </div>
+      </section>
+      
       <section id="sec-aboutme">
-        <h1>About Me</h1>
+        <div id="aboutme-grid-container">
+          <h1>About Me</h1>
+          <p>Some languages that I know and industry tools</p>
+        </div>
+        
       </section>
 
       <section id="sec-exp">
+
         <h1>Experience</h1>
+        <div id="exp-grid-container">
+          <p> Work experience</p>
+        </div>
+
       </section>
 
       <section id="sec-proj">
       <h1>Projects</h1>
+        <div id="proj-grid-container">
+          <p>Personal Projects that I have done</p>
+        </div>
       </section>
 
       <footer>
